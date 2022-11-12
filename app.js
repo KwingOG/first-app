cd36074a2cb1b720b5062d609ea77f557e8d0400
KwingOG <jcruzrisso00@gmail.com>
2022-11-12 05:12:43 -0300

    feat: Added public and utilities folders

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require	('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+
+
 const app = express();
 
+const rootDir = require('./utilities/path');
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
@@ -11,11 +14,14 @@ const shopRoutes = require('./routes/shop.js');
 
 //Parser
 app.use(bodyParser.urlencoded({extended:false}));
+//Static method for presenting CSS
+app.use(express.static(path.join(rootDir, 'public')));
 
+//Routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use((req, res, next) => {
-	res.status(404).sendFile(path.join(__dirname, '../', 'views', '404.html'));
+	res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
+//Server
 app.listen(3000);
