61d5f247500dfee1b9bad938ced0ea6475c96369
KwingOG <jcruzrisso00@gmail.com>
2022-11-13 03:18:20 -0300

    feat: global share data

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(rootDir, 'public')));
 
 //Routes
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
 	res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
