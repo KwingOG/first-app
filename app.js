3cb3e9deab398ab60c8b217e6a6c9e156e52b653
KwingOG <jcruzrisso00@gmail.com>
2022-11-13 04:32:45 -0300

    refactor: changed css and added ejs

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+//Se declara express con el modulo 3rd party expressjs
 const express = require	('express');
+//Se declara bodyParser con el modulo 3rd party body-parser
 const bodyParser = require('body-parser');
+//Se declara path node core module
 const path = require('path');
 
-
-
-const app = express();
-
+//My exports
 const rootDir = require('./utilities/path');
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
+//Declaro app con el method express() que trae un obj
+const app = express();
 
+//Setea en especial key 'view engine' a 'ejs' como default template engine
+app.set('view engine', 'ejs');
 
 
 //Parser
@@ -17,11 +21,12 @@ app.use(bodyParser.urlencoded({extended:false}));
 //Static method for presenting CSS
 app.use(express.static(path.join(rootDir, 'public')));
 
+
 //Routes
 app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-	res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+	res.status(404).render('404', {pageTitle: 'Page not found'})
 });
 //Server
 app.listen(3000);
